d104c66ca66166c42fe1b1afe71951b31f45ce4a
SpReeDateD <webmaster@born2skate.de>
2021-02-01 12:05:03 +0100

    RC

--- [Arduino][ESP32] Node/ESP32_Node/ESP32_Node/ESP32_Node.ino
+++ [Arduino][ESP32] Node/ESP32_Node/ESP32_Node/ESP32_Node.ino
@@ -1,3 +1,4 @@
+#include "config.h"
 #include <MQTT.h>
 #include <MQTTClient.h>
 
@@ -18,35 +19,10 @@
 #define ARDUINOJSON_ENABLE_STD_STREAM 0 //Resolves VMciro IntelliSense Error
 #include <ArduinoJson.h>
 
-// SETUP vars
-// ----------
 // 2019-2021 (c) neXn-Systems
 
 //Node Information
 const char* nodeVersion = "5.0";
-
-//Connect to WiFi
-const char* ssid = "nxn-fritz";
-const char* password = "69469573606998461850";
-
-//Node Ethernet Information
-const char* nodeHostname = "nxn-nodeESP32-95";
-const String nodeIP = "192.168.1.95";
-const String inetDNSServer = "192.168.1.254";
-const String inetGateway = "192.168.1.254";
-const String inetSubnet = "255.255.255.0";
-
-//MQTT
-const char* mqtt_server = "192.168.1.106";
-const char* nodeTopic = "/neXn/433/nodes/";
-const char* commandTopic = "/neXn/433/commands/";
-const char* mqtt_username = "cdavid";
-const char* mqtt_password = "cdavid";
-const char* clientID = "node1";
-const char* capabilities[] = { "433", "WLAN", "MQTT" };
-
-//433 Switch DataPin
-const int trasmitterDataPin = 32;
 
 #pragma region  Helper Functions
 String SplitStringValue(String data, char separator, int index)
@@ -68,10 +44,10 @@ String SplitStringValue(String data, char separator, int index)
 #pragma endregion
 
 //IPAddress from String
-const IPAddress nodeIPa = IPAddress(SplitStringValue(nodeIP, '.', 0).toInt(), SplitStringValue(nodeIP, '.', 1).toInt(), SplitStringValue(nodeIP, '.', 2).toInt(), SplitStringValue(nodeIP, '.', 3).toInt());
-const IPAddress inetDNSServerA = IPAddress(SplitStringValue(inetDNSServer, '.', 0).toInt(), SplitStringValue(inetDNSServer, '.', 1).toInt(), SplitStringValue(inetDNSServer, '.', 2).toInt(), SplitStringValue(inetDNSServer, '.', 3).toInt());
-const IPAddress inetGatewayA = IPAddress(SplitStringValue(inetGateway, '.', 0).toInt(), SplitStringValue(inetGateway, '.', 1).toInt(), SplitStringValue(inetGateway, '.', 2).toInt(), SplitStringValue(inetGateway, '.', 3).toInt());
-const IPAddress inetSubnetA = IPAddress(SplitStringValue(inetSubnet, '.', 0).toInt(), SplitStringValue(inetSubnet, '.', 1).toInt(), SplitStringValue(inetSubnet, '.', 2).toInt(), SplitStringValue(inetSubnet, '.', 3).toInt());
+const IPAddress nodeIPa = IPAddress(SplitStringValue(WiFi_IP, '.', 0).toInt(), SplitStringValue(WiFi_IP, '.', 1).toInt(), SplitStringValue(WiFi_IP, '.', 2).toInt(), SplitStringValue(WiFi_IP, '.', 3).toInt());
+const IPAddress WiFi_DNSServerA = IPAddress(SplitStringValue(WiFi_DNSServer, '.', 0).toInt(), SplitStringValue(WiFi_DNSServer, '.', 1).toInt(), SplitStringValue(WiFi_DNSServer, '.', 2).toInt(), SplitStringValue(WiFi_DNSServer, '.', 3).toInt());
+const IPAddress inetGatewayA = IPAddress(SplitStringValue(WiFi_Gateway, '.', 0).toInt(), SplitStringValue(WiFi_Gateway, '.', 1).toInt(), SplitStringValue(WiFi_Gateway, '.', 2).toInt(), SplitStringValue(WiFi_Gateway, '.', 3).toInt());
+const IPAddress inetSubnetA = IPAddress(SplitStringValue(WiFi_Subnet, '.', 0).toInt(), SplitStringValue(WiFi_Subnet, '.', 1).toInt(), SplitStringValue(WiFi_Subnet, '.', 2).toInt(), SplitStringValue(WiFi_Subnet, '.', 3).toInt());
 
 //RCSwitch
 RCSwitch nxnSwitch = RCSwitch();
@@ -134,40 +110,77 @@ const char* command;
 bool state;
 const char* homecode;
 const char* socket;
+int genuineNum1;
+int genuineNum2;
 //# ### #
+
+void MQTT_ClearDVars()
+{
+    for (size_t i = 0; i < sizeof(jsonClients)/sizeof(jsonClients[0]); i++)
+    {
+        jsonClients[i] = NULL;
+    }
+    command = "\0";
+    homecode = "\0";
+    socket = "\0";
+    state = 0;
+    genuineNum1 = 0;
+    genuineNum2 = 0;
+}
 
 void MQTT_OnReceive(String& topic, String& payload) 
 {
+    MQTT_ClearDVars();
+
     Serial.print("| [MQTT] Message Received - Topic \"");
     Serial.print(topic);
     Serial.print("\" - Payload \"");
     Serial.print(payload);
     Serial.println("\"");
 
+    //De-Serialize
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, payload);
 
-    state = doc["state"];
-    homecode = doc["homecode"];
-    socket = doc["socket"];
-
     int arraySize = doc["clients"].size();
 
-   for (int i = 0; i < arraySize; i++) {
+    for (int i = 0; i < arraySize; i++) {
 
         jsonClients[i] = doc["clients"][i];
     }
 
-    Serial.println("State:" + String(state));
-    Serial.println("HomeCode:" + String(homecode));
-    Serial.println("Socket:" + String(socket));
-    Serial.println("ASize:" + String(arraySize));
-    Serial.println("Array0:" + String(jsonClients[0]));
-
-    if (arraySize > 0 && KeyWordComparison(jsonClients, arraySize, clientID))
+    if (arraySize <= 0 || !KeyWordComparison(jsonClients, arraySize, clientID)) //Is this message for us?
     {
-        Serial.println("Yes ME!");
+        Serial.println("| [MQTT] Message not for me, won't process.");
+        return;
     }
+
+    state = doc["state"];
+    homecode = doc["homecode"];
+    socket = doc["socket"];
+    command = doc["command"];
+    genuineNum1 = doc["gNum1"];
+    genuineNum2 = doc["gNum2"];
+    //# ### #
+
+    //Process commands
+    if (String(command) == "switch" || String(command) == "socket")
+    {
+        Command_SwitchSocket(homecode, socket, state);
+        return;
+    }
+    if (String(command) == "reset" || String(command) == "restart")
+    {
+        Command_Reset();
+        return;
+    }
+    if ((String(command) == "original" || String(command) == "genuine") && genuineNum1 != 0 && genuineNum2 != 0)
+    {
+        Command_IsGenuine(genuineNum1, genuineNum2);
+        return;
+    }
+
+    Serial.println("| [MQTT] Unknown command or command missing parameters");
 }
 
 bool KeyWordComparison(const char * haystack[], int sizeOfHaystack, const char * needle) 
@@ -186,102 +199,70 @@ bool KeyWordComparison(const char * haystack[], int sizeOfHaystack, const char *
 #pragma region Command routines
 void Command_Reset() 
 {
-    //if (String(rcvd) == "nxn#reset") {
-    //    //Answer
-    //    Serial.println("Restarting device...");
-    //    SendUDPMessage("Restarting device...", &UDPListener);
-    //    UDPListener.stop();
-    //    //# ### #
-    //    Serial.println("5 seconds...");
-    //    delay(1000);
-    //    Serial.println("4 seconds...");
-    //    delay(1000);
-    //    Serial.println("3 seconds...");
-    //    delay(1000);
-    //    Serial.println("2 seconds...");
-    //    delay(1000);
-    //    Serial.println("1 seconds...");
-    //    delay(1000);
-    //    Serial.println("Restarting... please stand by.");
-    //    ESP.restart();
-    //}
+    Serial.println("| [MQTT] Executing Command \"Restart\"");
+    Serial.println("5 seconds...");
+    delay(1000);
+    Serial.println("4 seconds...");
+    delay(1000);
+    Serial.println("3 seconds...");
+    delay(1000);
+    Serial.println("2 seconds...");
+    delay(1000);
+    Serial.println("1 second...");
+    delay(1000);
+    Serial.println("Restarting... please stand by.");
+    ESP.restart();
 }
 
-bool Command_IsOriginal() 
+bool Command_IsGenuine(int dividend, int divisor)
 {
-    //if (rcvd.startsWith("nxn#") && rcvd.length() > 4) {
-    //    int dividend = SplitStringValue(rcvd, '#', 1).toInt();
-    //    int divisor = SplitStringValue(rcvd, '#', 2).toInt();
-    //    if (dividend % divisor == 1337) { //nxn#2675#1338
-    //        char buf[100];
-    //        strcpy(buf, nodeHostname);
-    //        strcpy(buf, " operating on nominal parameters");
-    //        Serial.println("Genuine neXn-Systems device.");
-    //        //Serial.println(buf);
-    //        Serial.println("Firmware Version: " + nodeVersion);
-    //        Serial.println(UDPListener.remoteIP());
-    //        Serial.println(UDPListener.remotePort());
-    //        //Answer
-    //        //SendUDPMessage("Genuine neXn-Systems device#" + nodeVersion, &UDPListener);
-    //        //# ### #
-    //        return;
-    //    }
-    //}
+    if (dividend % divisor == 1337) { //nxn#2675#1338
+        Serial.println("Genuine neXn-Systems device.");
+        Serial.println("Firmware Version: " + String(nodeVersion));
+        MQTT_Publish("{\"client\":\"" + String(clientID) + "\", \"info\":\"Genuine neXn-Systems device\", \"firmware\":\"" + String(nodeVersion) + "\"}");
+        return true;
+    }
+    return false;
 }
 
-void Command_SwitchSocket()
+void Command_SwitchSocket(const char* homecode, const char* socket, bool state)
 {
-    //String homecode = SplitStringValue(String(rcvd), '#', 0);
-    //String socket = SplitStringValue(String(rcvd), '#', 1);
-    //String onORoff = SplitStringValue(String(rcvd), '#', 2);
+    //Legacy support
+    if (String(socket) == "1") { socket = "10000"; }
+    if (String(socket) == "2") { socket = "01000"; }
+    if (String(socket) == "3") { socket = "00100"; }
+    if (String(socket) == "4") { socket = "00010"; }
+    if (String(socket) == "5") { socket = "00001"; }
+    //# ### #
 
-    ////Legacy
-    //if (socket == "1") {
-    //    socket = "10000";
-    //}
-    //if (socket == "2") {
-    //    socket = "01000";
-    //}
-    //if (socket == "3") {
-    //    socket = "00100";
-    //}
-    //if (socket == "4") {
-    //    socket = "00010";
-    //}
-    //if (socket == "5") {
-    //    socket = "00001";
-    //}
-    ////# ### #
+    if (state) 
+    {
+        nxnSwitch.switchOn(homecode, socket);
+    }
+    else
+    {
+        nxnSwitch.switchOff(homecode, socket);
+    }
 
-    //if (onORoff == "0") {
-    //    //SendUDPMessage("OFF#" + homecode + "#" + socket, &UDPListener);
-    //    //Send 5 times
-    //    for (int i = 0; i <= 4; i++) {
-    //        nxnSwitch.switchOff((char*)homecode.c_str(), (char*)socket.c_str());
-    //        delay(50);
-    //    }
-    //    Serial.println("OFF - " + homecode + " " + socket);
-    //}
-    //else if (onORoff == "1") {
-    //    //SendUDPMessage("ON#" + homecode + "#" + socket, &UDPListener);
-    //    for (int i = 0; i <= 4; i++) {
-    //        nxnSwitch.switchOn((char*)homecode.c_str(), (char*)socket.c_str());
-    //        delay(50);
-    //    }
-    //    Serial.println("ON - " + homecode + " " + socket);
-    //}
+    Serial.print("| [SwitchSocket] Homecode \"");
+    Serial.print(homecode);
+    Serial.print("\" - Socket \"");
+    Serial.print(socket);
+    Serial.print("\" - State \"");
+    Serial.print(state);
+    Serial.println("\"");
 }
 #pragma endregion
 
 #pragma region  WiFi Handling
 void WiFiSetup()
 {
-    Serial.printf("| [WiFi] Connecting to %s ", ssid);
+    Serial.printf("| [WiFi] Connecting to %s ", WiFi_SSID);
     WiFi.mode(WIFI_STA);
-    WiFi.config(nodeIPa, inetDNSServerA, inetGatewayA, inetSubnetA);
+    WiFi.config(nodeIPa, WiFi_DNSServerA, inetGatewayA, inetSubnetA);
     WiFi.setAutoReconnect(true);
-    WiFi.begin(ssid, password);
-    WiFi.setHostname(nodeHostname); //Set Hostname AFTER WiFi.begin();
+    WiFi.begin(WiFi_SSID, WiFi_Password);
+    WiFi.setHostname(WiFi_Hostname); //Set Hostname AFTER WiFi.begin();
     WiFiConnect();
 }
 int wifiRetries = 0;
@@ -316,7 +297,7 @@ void setup() {
     Serial.print("Welcome to the neXn-Systems NodeMCU v");
     Serial.println(nodeVersion);
     Serial.println("| [NodeInfo] Node MAC: " + WiFi.macAddress());
-    Serial.println("| [NodeInfo] Node IP: " + nodeIP);
+    Serial.println("| [NodeInfo] Node IP: " + WiFi_IP);
 
     //WiFi Connection
     WiFiSetup();
