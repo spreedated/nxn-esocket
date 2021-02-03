bb9b9bd49f125db6cba5db1c8ab6b3229aa7db8c
SpReeDateD <webmaster@born2skate.de>
2021-02-03 23:11:23 +0100

    compatibility with esp8266

--- [Arduino][ESP32] Node/NodeMCU/NodeMCU/NodeMCU.ino
+++ [Arduino][ESP32] Node/NodeMCU/NodeMCU/NodeMCU.ino
@@ -368,8 +368,7 @@ void WiFiReconnect()
 #pragma region OTA Update
 void StartOTA()
 {
-    ArduinoOTA
-        .onStart([]() {
+    ArduinoOTA.onStart([]() {
         String type;
         if (ArduinoOTA.getCommand() == U_FLASH)
             type = "sketch";
@@ -378,25 +377,29 @@ void StartOTA()
 
         // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
         Serial.println("| [OTA] Start updating " + type);
-            })
-        .onEnd([]() {
-                Serial.println("\n| [OTA] End");
-            })
-        .onProgress([](unsigned int progress, unsigned int total) {
+        });
+
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
         Serial.printf("| [OTA] Progress: %u%%\n", (progress / (total / 100)));
-            })
-        .onError([](ota_error_t error) {
+        });
+
+    ArduinoOTA.onError([](ota_error_t error) {
                 Serial.printf("| [OTA] Error[%u]: ", error);
                 if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
                 else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
                 else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
                 else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
                 else if (error == OTA_END_ERROR) Serial.println("End Failed");
-            });
 
-        ArduinoOTA.begin();
+        });
 
-        Serial.println("| [OTA] Ready!");
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\n| [OTA] End");
+        });
+
+    ArduinoOTA.begin();
+
+    Serial.println("| [OTA] Ready!");
 }
 #pragma endregion
 
